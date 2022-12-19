aa4a8a656c00a08ca91890d9c45e4f90d8188052
abdurrahman <abdurrahman@DESKTOP-NO74JF9>
2022-12-19 03:30:25 -0800

    Add project files.

---
+++ Web-Homework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Homework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
